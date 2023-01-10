7c90914d379c099a38eb564f69fd75a5a9e8707f
mramadhanurh <mramadhanur@gmail.com>
2023-01-10 21:11:33 +0700

    variable & tipe data

---
+++ bin/variable_tipedata.dart
@@ -0,0 +1,30 @@
+void main(List<String> arguments) {
+  // variable tipe 1
+  //var nama = "mramadhanurh";
+
+  // variable tipe 2
+  //var nama;
+  //nama = "Hives";
+
+  //print(nama);
+
+  // tipe data
+  // int => 1,2,10,100
+  //var angka = 1;
+  //print(angka);
+
+  // double => 1.2, 5.0, 10.1
+  //var angka2 = 2.6;
+  //print(angka2);
+
+  // boolean => true & false
+  //var perbandingan = true;
+
+  // string => kalimat
+  var namaDepan = 'mrama';
+  var namaBelakang = 'dhanurh';
+
+  var kalimat =
+      '$namaDepan $namaBelakang membeli gorengan seharga \$2 di hari jum\'at';
+  print(kalimat);
+}
